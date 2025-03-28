0149b0c0f94669e466e3ab1ee832f2eafb54ec57
?kxkxk_q <sukhanov.is@phystech.edu>
2025-03-29 00:19:06 +0300

    [driver] add symbol table implementation

--- include/driver.hpp
+++ include/driver.hpp
@@ -5,9 +5,13 @@
 //-------------------------------------------------------------------------------------------------
 #pragma once
 
+#include <algorithm>
+#include <cassert>
 #include <iostream>
-#include <vector>
+#include <stack>
+#include <type_traits>
 #include <utility>
+#include <vector>
 
 #include <FlexLexer.h>
 
@@ -22,6 +26,7 @@ namespace yy
         FlexLexer *pLexer_;
         ast::Builder astBuilder_;
         ast::CurrentScopeNode* ast_;
+        std::vector<CurrentScopeNode*> scopeStorage; 
 
     public :
         Driver(FlexLexer *l) : pLexer_(l) {};
@@ -56,10 +61,64 @@ namespace yy
             ast_ = curScope;
         }
 
+        CurrentScopeNode* get_current_scope() { assert(scopeStorage.back()); return scopeStorage.back(); }
+
         template <typename NodeType, class... Args>
-        NodeType* make_node(Args&&... args)
+        NodeType* make_node(Args&&... args) { return astBuilder_.make_node<NodeType>(args ...); }
+
+        void descend_into_scope(CurrentScopeNode* currScope)
         {
-            return astBuilder_.make_node<NodeType>(args ...);
+            std::cerr << "descend_into_scope()" << std::endl;
+            std::cerr << "!!  DEBUG INFO : " << std::endl;
+            if(!scopeStorage.empty())
+                std::cerr << "previous scope : " << scopeStorage.back() << std::endl;
+            std::cerr << "current scope : " << currScope << std::endl;
+            assert(currScope);
+            scopeStorage.emplace_back(currScope);
+            assert(currScope == scopeStorage.back());
+        }
+
+        void ascend_from_scope()
+        {
+            std::cerr << "ascend_from_scope()" << std::endl;
+            assert(!scopeStorage.empty());
+            scopeStorage.pop_back();
+        }
+
+        void add_to_context(VariableNode* var)
+        {
+            assert(var);
+            try
+            {
+                scopeStorage.back()->add_to_context(var);
+            }
+            catch(...)
+            {
+                //  
+            }
+        }
+        
+        VariableNode* find_variable(const std::string id) const
+        {
+            auto is_declared = [&id] (CurrentScopeNode* sc) { return sc->is_declared(id); };
+            auto iter = std::find_if(scopeStorage.rbegin(), scopeStorage.rend(), is_declared);
+            
+            if(iter == scopeStorage.rend())
+                return nullptr;
+            return (*iter)->get_variable(id);
+        }
+
+        bool is_variable_declared(const std::string id) const { return find_variable(id) != nullptr; }
+
+        template <typename NodeType, class... Args>
+        NodeType* make_or_assign(Args&&... args)
+        {
+            if(std::is_same_v<NodeType, ast::VariableNode>)
+            {
+                if(is_variable_declared(args ...))   
+                    return find_variable(args ...);
+            }
+            return make_node<NodeType>(args ...);
         }
 
         void execute()
